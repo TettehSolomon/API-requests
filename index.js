fca59f9ec9661a1545ffb79af9cef129e2c0f1ec
TettehSolomon <tettehsolomon106@gmail.com>
2025-03-03 20:42:28 +0100

    API requests

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //fetch - a JavaScript function used in making http requests to fetch resources
 
 fetch("https://pokeapi.co/api/v2/pokemon")
-.then(response => console.log(response))
+.then(response => response.json())
+.then(data => console.log(data.results[0]))
 .catch(error => console.error(error))
